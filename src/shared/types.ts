a7aa53154f74068777eda8c73d5143f6235f3db1
sudarshan1212 <sudarshansam33@gmail.com>
2024-08-02 12:52:33 +0530

    final

---
+++ src/shared/types.ts
@@ -0,0 +1,17 @@
+export enum SelectedPage {
+  Home = "home",
+  Benefits = "benfits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+}
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
